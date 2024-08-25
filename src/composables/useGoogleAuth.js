48495092f379ae44f4dc5dc894f456aea46a8596
ihkaru <ihza2karunia@gmail.com>
2024-08-25 17:11:23 +0700

    test

--- src/composables/useGoogleAuth.js
+++ src/composables/useGoogleAuth.js
@@ -2,6 +2,8 @@ import { ref } from 'vue'
 import axios from 'axios'
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
+axios.defaults.headers["Access-Control-Allow-Methods"] = "GET, POST, PATCH, PUT, DELETE, OPTIONS";
+axios.defaults.headers["Access-Control-Allow-Headers"] = "Origin, Content-Type, X-Auth-Token";
 axios.defaults.withCredentials = true
 const api = axios.create({
   baseURL: process.env.QENV_API_URL
